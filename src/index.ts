96e2adc902d8885ef29b13f49c45d4ccb2dfe937
Manmohan Wable <manmohanwable0@gmail.com>
2025-05-19 10:08:38 +0530

    updated .gitignore and Removed Comments

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const start = async () => {
-  // 1. Check environment variables
   if (!process.env.SECRET_KEY) {
     throw new Error('SECRET_KEY must be defined');
   }
@@ -15,10 +14,9 @@ const start = async () => {
     
   } catch (err) {
     console.error(err);
-    process.exit(1); // crash if cannot connect
+    process.exit(1);
   }
 
-  // 3. Start server
   app.listen(3002, () => {
     console.log('Auth Service listening on port 3002');
   });
